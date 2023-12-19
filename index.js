50e2072eb423ebc005047fa75973ec6aee25306c
Oliver Marler <103157332+rocklandceo@users.noreply.github.com>
2023-12-19 07:11:02 -0800

    Inital Commit

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
-const fs = require('fs')
-const YoutubeMp3Downloader = require('youtube-mp3-downloader')
-const { Deepgram } = require('@deepgram/sdk')
-const ffmpeg = require('ffmpeg-static')
+const fs = require('fs');
+const readline = require('readline'); // Import readline module
+const YoutubeMp3Downloader = require('youtube-mp3-downloader');
+const { Deepgram } = require('@deepgram/sdk');
+const ffmpeg = require('ffmpeg-static');
 
-const deepgram = new Deepgram(process.env.DG_KEY)
-const YD = new YoutubeMp3Downloader({
-  ffmpegPath: ffmpeg,
-  outputPath: './',
-  youtubeVideoQuality: 'highestaudio'
-})
+const deepgram = new Deepgram(process.env.DG_KEY);
 
-YD.download('ir-mWUYH_uo')
+// Create a readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-YD.on('progress', data => {
-  console.log(data.progress.percentage + '% downloaded')
-})
+// Prompt the user to enter the YouTube URL
+rl.question('Please enter the YouTube URL: ', (url) => {
+  const YD = new YoutubeMp3Downloader({
+    ffmpegPath: ffmpeg,
+    outputPath: './',
+    youtubeVideoQuality: 'highestaudio'
+  });
 
-YD.on('finished', async (err, video) => {
-  const videoFileName = video.file
-  console.log(`Downloaded ${videoFileName}`)
+  YD.download(url); // Use the user-provided URL
 
-  const file = {
-    buffer: fs.readFileSync(videoFileName),
-    mimetype: 'audio/mp3'
-  }
-  const options = {
-    punctuate: true
-  }
+  YD.on('progress', data => {
+    console.log(data.progress.percentage + '% downloaded');
+  });
 
-  const result = await deepgram.transcription.preRecorded(file, options).catch(e => console.log(e))
-  const transcript = result.results.channels[0].alternatives[0].transcript
+  YD.on('finished', async (err, video) => {
+    const videoFileName = video.file;
+    console.log(`Downloaded ${videoFileName}`);
 
-  fs.writeFileSync(`${videoFileName}.txt`, transcript, () => `Wrote ${videoFileName}.txt`)
-  fs.unlinkSync(videoFileName)
-})
+    const file = {
+      buffer: fs.readFileSync(videoFileName),
+      mimetype: 'audio/mp3'
+    };
+    const options = {
+      punctuate: true
+    };
+
+    const result = await deepgram.transcription.preRecorded(file, options).catch(e => console.log(e));
+    const transcript = result.results.channels[0].alternatives[0].transcript;
+
+    fs.writeFileSync(`${videoFileName}.txt`, transcript, () => `Wrote ${videoFileName}.txt`);
+    fs.unlinkSync(videoFileName);
+
+    // Close the readline interface
+    rl.close();
+  });
+});
